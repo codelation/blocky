2fa0856a2a8a5e70fb038118c52b4e594f61f687
Brian Pattison <brian@brianpattison.com>
2014-05-20 15:19:46 -0500

    Fix install generator

--- lib/generators/blocky/install_generator.rb
+++ lib/generators/blocky/install_generator.rb
@@ -1,94 +1,41 @@
-module Blogelator
+module Blocky
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("../../../..", __FILE__)
-    
+
     def ask_questions
-      @site_name = ask("What is the name of your blog? [Blogelator]")
-      if @site_name.blank?
-        @site_name = "Blogelator"
+      @blocky_route = ask("What route should be used to manage content blocks? [/admin/content]")
+      if @blocky_route.blank?
+        @blocky_route = "/admin/content"
       end
-      
-      @blogelator_route = ask("What route should be used to access the blog? [/blog]")
-      if @blogelator_route.blank?
-        @blogelator_route = "/blog"
-      end
-      
+
       @user_class = ask("What class is used for user authentication? [User]")
       if @user_class.blank?
         @user_class = "User"
       end
     end
-    
+
     def create_initializer_file
-      create_file "config/initializers/blogelator.rb", <<-INITIALIZER
-Blogelator.posts_per_page       = 5
-Blogelator.site_name            = "#{@site_name}"
-Blogelator.user_class           = "#{@user_class}"
-Blogelator.s3_access_key_id     = ENV["BLOGELATOR_S3_KEY"]
-Blogelator.s3_secret_access_key = ENV["BLOGELATOR_S3_SECRET"]
-Blogelator.s3_bucket            = ENV["BLOGELATOR_S3_BUCKET"]
+      create_file "config/initializers/blocky.rb", <<-INITIALIZER
+Blocky.user_class           = "#{@user_class}"
+Blocky.s3_access_key_id     = ENV["BLOCKY_S3_KEY"]
+Blocky.s3_secret_access_key = ENV["BLOCKY_S3_SECRET"]
+Blocky.s3_bucket            = ENV["BLOCKY_S3_BUCKET"]
 INITIALIZER
     end
-    
-    def create_stylesheets
-      create_file "app/assets/stylesheets/blogelator/application.css.scss", <<-APPLICATIONCSS
-/*
- *= require blogelator/application/all
- *= require_self
- */
-APPLICATIONCSS
 
-      create_file "app/assets/stylesheets/blogelator/admin.css.scss", <<-ADMINCSS
-/*
- *= require blogelator/admin/all
- *= require_self
- */
-ADMINCSS
-    end
-    
-    def copy_variables_file
-      puts File.expand_path("../../../..", __FILE__)
-      source = "lib/assets/stylesheets/blogelator/_variables_sample.scss"
-      destination = "app/assets/stylesheets/blogelator/_variables.scss"
-      copy_file source, destination
-    end
-    
-    def create_overrides_directory
-      empty_directory "app/overrides"
-    end
-    
-    def configure_application
-      application <<-APP
-
-    config.to_prepare do
-      # Load application's model / class decorators
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-
-      # Load application's view overrides
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-      APP
-    end
-    
     def install_migrations
-      rake "blogelator:install:migrations"
+      rake "blocky:install:migrations"
     end
-    
+
     def mount_engine
       insert_into_file(File.join("config", "routes.rb"), :after => "Application.routes.draw do\n") do <<-MOUNTENGINE
-  # This line mounts Blogelator's routes to the path '#{@blogelator_route}'.
-  # This means, any requests to '#{@blogelator_route}', will go to Blogelator::PostsController.
+  # This line mounts Blocky's routes to the path '#{@blocky_route}'.
+  # This means, any requests to '#{@blocky_route}', will go to Blocky::ContentBlocksController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Blogelator relies on it being the default of "blogelator"
-  mount Blogelator::Engine, at: "#{@blogelator_route}"
+  mount Blocky::Engine, at: "#{@blocky_route}"
         MOUNTENGINE
       end
     end
-    
+
   end
 end
