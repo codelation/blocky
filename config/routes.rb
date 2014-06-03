4268c90388a759ca915a41e8d3b47cd95315141e
Matthew Sullivan <msull92@gmail.com>
2014-06-03 16:52:53 -0500

    jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Blocky::Engine.routes.draw do
   resources :content_blocks, path: "/"
+  resources :content_blocks, path: "/api/content_blocks"
   resources :images
   root to: "content_blocks#index"
 end
