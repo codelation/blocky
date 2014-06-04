2db57906c7a120de5165f59c2b149fac6e433a8f
Matthew Sullivan <msull92@gmail.com>
2014-06-04 17:22:35 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Blocky::Engine.routes.draw do
   resources :content_blocks, path: "/"
-  resources :content_blocks, path: "/api/content_blocks"
   resources :images
   root to: "content_blocks#index"
 end
